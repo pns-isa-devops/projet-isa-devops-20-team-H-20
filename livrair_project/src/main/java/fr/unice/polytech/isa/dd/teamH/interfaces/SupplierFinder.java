eac4d3c65c1f694d74b919e7007d8ccc9cef16c4
Montoya Damien <damien.montoya26@gmail.com>
2020-03-06 10:59:50 +0100

    package service + finder #8

--- livrair_project/src/main/java/fr/unice/polytech/isa/dd/teamH/interfaces/SupplierFinder.java
+++ livrair_project/src/main/java/fr/unice/polytech/isa/dd/teamH/interfaces/SupplierFinder.java
@@ -1,8 +1,9 @@
 package fr.unice.polytech.isa.dd.teamH.interfaces;
 
 import fr.unice.polytech.isa.dd.teamH.entities.Supplier;
-import fr.unice.polytech.isa.dd.teamH.exceptions.SupplierNotExistsException;
+
+import java.util.Optional;
 
 public interface SupplierFinder {
-    Supplier findByName(String name) throws SupplierNotExistsException;
+    Optional<Supplier> findByName(String name);
 }
