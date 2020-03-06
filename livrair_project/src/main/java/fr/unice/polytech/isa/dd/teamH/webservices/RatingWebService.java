a1c1e4c2429ca39c82685a7dbceeb541e3c957bb
Francois Rigaut <francois.rigaut@etu.unice.fr>
2020-03-06 09:35:03 +0100

    RatingWebService basic implementation for structure example

---
+++ livrair_project/src/main/java/fr/unice/polytech/isa/dd/teamH/webservices/RatingWebService.java
@@ -0,0 +1,17 @@
+package fr.unice.polytech.isa.dd.teamH.webservices;
+
+import fr.unice.polytech.isa.dd.teamH.exceptions.DeliveryNotExistsException;
+
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService(targetNamespace = "http://www.polytech.unice.fr/si/4a/isa/dd/team-h/rating")
+public interface RatingWebService {
+
+    @WebMethod
+    void createComment(@WebParam(name="packageTrackingNumber") String packageTrackingNumber,
+                       @WebParam(name="rating") int rating,
+                       @WebParam(name="content") String content)
+            throws DeliveryNotExistsException;
+}
---
+++ livrair_project/src/main/java/fr/unice/polytech/isa/dd/teamH/webservices/RatingWebServiceImpl.java
@@ -0,0 +1,22 @@
+package fr.unice.polytech.isa.dd.teamH.webservices;
+
+import fr.unice.polytech.isa.dd.teamH.exceptions.DeliveryNotExistsException;
+import fr.unice.polytech.isa.dd.teamH.interfaces.DeliveryFinder;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+@WebService(targetNamespace = "http://www.polytech.unice.fr/si/4a/isa/dd/team-h/rating")
+@Stateless(name = "RatingWS")
+public class RatingWebServiceImpl implements RatingWebService {
+
+    @EJB
+    private DeliveryFinder finder;
+
+    @Override
+    public void createComment(String packageTrackingNumber, int rating, String content)
+            throws DeliveryNotExistsException {
+
+    }
+}
