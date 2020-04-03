93b7ec30e39b430e3105c7ba0aef639730ee322d
Florian Focas <florianfocas@gmail.com>
2020-04-03 10:36:44 +0200

    #51 fixed address

--- clients/Boss/src/main/java/api/DronePublicAPI.java
+++ clients/Boss/src/main/java/api/DronePublicAPI.java
@@ -18,7 +18,7 @@ public class DronePublicAPI {
         URL wsdlLocation = DronePublicAPI.class.getResource("/StatsWebServiceImpl.wsdl");
         StatsWebServiceImplService factory = new StatsWebServiceImplService(wsdlLocation);
         this.statisticsWebService = factory.getStatsWebServiceImplPort();
-        String address = "http://" + host + ":" + port + "/drone-delivery-backend/webservices/DroneWS";
+        String address = "http://" + host + ":" + port + "/drone-delivery-backend/webservices/StatsWS";
         //((BindingProvider) statisticsWebService).getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, address);
     }
 
