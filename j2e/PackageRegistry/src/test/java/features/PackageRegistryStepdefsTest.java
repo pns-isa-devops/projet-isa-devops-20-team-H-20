19b48dec9f9dc9e8ecbea32a71c0ecef5454f2bf
Damien Montoya <daminor26@gmail.com>
2020-04-12 00:14:21 +0200

    more cucumber tests added for package #55

--- j2e/PackageRegistry/src/test/java/features/PackageRegistryStepdefsTest.java
+++ j2e/PackageRegistry/src/test/java/features/PackageRegistryStepdefsTest.java
@@ -20,12 +20,10 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 import javax.ejb.EJB;
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.*;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
@@ -38,9 +36,6 @@ public class PackageRegistryStepdefsTest extends AbstractPackageRegistryTest {
 
     @EJB private SupplierRegistration supplierRegistration;
     @EJB private SupplierFinder supplierFinder;
-
-    @Inject private UserTransaction utx;
-    @PersistenceContext private EntityManager entityManager;
 
     private Exception exception = null;
     private Optional<Package> packageFound;
@@ -65,22 +60,21 @@ public class PackageRegistryStepdefsTest extends AbstractPackageRegistryTest {
             fail();
     }
 
-    //@Given("^some suppliers (.*) and (.*) and packages with trackingIds (.*) (.*)$")
-    //public void background(String supplier, String supplier2, String trackingId, String trackingId2) throws Exception{
-    //    management.flush();
-    //    supplierRegistration.flush();
-    //    supplierRegistration.register(supplier, "0649712258");
-    //    supplierRegistration.register(supplier2, "0649712255");
-//
-    //    Optional<Supplier> supplierObject = supplierFinder.findByName(supplier);
-    //    Optional<Supplier> supplierObject2 = supplierFinder.findByName(supplier);
-    //    if(!supplierObject.isPresent() || !supplierObject2.isPresent())
-    //        fail();
-//
-    //    management.register(trackingId, supplierObject.get(), 5.0f, "Nice ville");
-    //    management.register(trackingId2, supplierObject.get(), 8.0f, "Nice ville");
-    //    assertEquals(2, finder.findAllPackages().size());
-    //}
+    @Given("^some suppliers (.*) and (.*) and packages with trackingIds (.*) (.*)$")
+    public void background(String supplier, String supplier2, String trackingId, String trackingId2) throws Exception{
+        packagesToDelete = new HashSet<>();
+        suppliersToDelete = new HashSet<>();
+        suppliersToDelete.add(supplierRegistration.register(supplier, "0649712258"));
+        suppliersToDelete.add(supplierRegistration.register(supplier2, "0649712255"));
+
+        Optional<Supplier> supplierObject = supplierFinder.findByName(supplier);
+        Optional<Supplier> supplierObject2 = supplierFinder.findByName(supplier);
+        if(!supplierObject.isPresent() || !supplierObject2.isPresent())
+            fail();
+
+        packagesToDelete.add(management.register(trackingId, supplierObject.get(), 5.0f, "Nice ville"));
+        packagesToDelete.add(management.register(trackingId2, supplierObject.get(), 8.0f, "Nice ville"));
+    }
 
     @When("^the manutentionnaire adds the package with trackingId (.*) and (.*) as supplier$")
     public void addPackage(String trackingId, String supplier) throws Exception{
@@ -101,30 +95,33 @@ public class PackageRegistryStepdefsTest extends AbstractPackageRegistryTest {
         packagesToDelete.add(management.register(trackingId2, supplierObject.get(), 7.0f, "Biota"));
     }
 
-    //@When("^the manutentionnaire deletes the package with trackingId (.*)$")
-    //public void deletePackage(String trackingId) throws Exception{
-    //    management.delete(trackingId);
-    //}
-//
-    //@When("^the manutentionnaire deletes the packages with trackingId (.*) and (.*)$")
-    //public void deletePackages(String trackingId, String trackingId2) throws Exception{
-    //    management.delete(trackingId);
-    //    management.delete(trackingId2);
-    //}
-//
-    //@When("^the manutentionnaire search the package with id (.*)$")
-    //public void searchPackages(String trackingId){
-    //    packageFound = finder.findPackageByTrackingNumber(trackingId);
-    //}
-//
-    //@When("^the manutentionnaire edit the package with trackingId (.*) and set (.*) as supplier$")
-    //public void editPackage(String trackingId, String supplier) throws Exception{
-    //    Optional<Supplier> supplierObject = supplierFinder.findByName(supplier);
-    //    if(!supplierObject.isPresent())
-    //        fail();
-//
-    //    management.edit(trackingId, supplierObject.get(), 6.0f, "Nice ville");
-    //}
+    @When("^the manutentionnaire deletes the package with trackingId (.*)$")
+    public void deletePackage(String trackingId) throws Exception{
+        management.delete(trackingId);
+        packagesToDelete.removeIf(e -> e.getTrackingNumber().equals(trackingId));
+    }
+
+    @When("^the manutentionnaire deletes the packages with trackingId (.*) and (.*)$")
+    public void deletePackages(String trackingId, String trackingId2) throws Exception{
+        management.delete(trackingId);
+        management.delete(trackingId2);
+        packagesToDelete.removeIf(e -> e.getTrackingNumber().equals(trackingId));
+        packagesToDelete.removeIf(e -> e.getTrackingNumber().equals(trackingId2));
+    }
+
+    @When("^the manutentionnaire search the package with id (.*)$")
+    public void searchPackages(String trackingId){
+        packageFound = finder.findPackageByTrackingNumber(trackingId);
+    }
+
+    @When("^the manutentionnaire edit the package with trackingId (.*) and set (.*) as supplier$")
+    public void editPackage(String trackingId, String supplier) throws Exception{
+        Optional<Supplier> supplierObject = supplierFinder.findByName(supplier);
+        if(!supplierObject.isPresent())
+            fail();
+
+        management.edit(trackingId, supplierObject.get(), 6.0f, "Nice ville");
+    }
 
     @When("^the gestionnaire wants to add the package with trackingId (.*) and supplier (.*) there is an error$")
     public void addPackageError(String trackingId, String supplier){
@@ -139,27 +136,27 @@ public class PackageRegistryStepdefsTest extends AbstractPackageRegistryTest {
         }
     }
 
-    //@When("^the gestionnaire wants to delete the package with trackingId (.*) there is an error$")
-    //public void deletePackageError(String trackingId){
-    //    try {
-    //        management.delete(trackingId);
-    //    }catch (UnknownPackageException e){
-    //        exception = e;
-    //    }
-    //}
-//
-    //@When("^the gestionnaire wants to edit the package with trackingId (.*) and supplier (.*) there is an error$")
-    //public void editPackageError(String trackingId, String supplier){
-    //    Optional<Supplier> supplierObject = supplierFinder.findByName(supplier);
-    //    if(!supplierObject.isPresent())
-    //        fail();
-//
-    //    try {
-    //        management.edit(trackingId, supplierObject.get(), 5.2f, "Nice ville");
-    //    }catch (UnknownPackageException e){
-    //        exception = e;
-    //    }
-    //}
+    @When("^the gestionnaire wants to delete the package with trackingId (.*) there is an error$")
+    public void deletePackageError(String trackingId){
+        try {
+            management.delete(trackingId);
+        }catch (UnknownPackageException e){
+            exception = e;
+        }
+    }
+
+    @When("^the gestionnaire wants to edit the package with trackingId (.*) and supplier (.*) there is an error$")
+    public void editPackageError(String trackingId, String supplier){
+        Optional<Supplier> supplierObject = supplierFinder.findByName(supplier);
+        if(!supplierObject.isPresent())
+            fail();
+
+        try {
+            management.edit(trackingId, supplierObject.get(), 5.2f, "Nice ville");
+        }catch (UnknownPackageException e){
+            exception = e;
+        }
+    }
 
     @Then("^there is an exception$")
     public void checkExceptionAdd(){
@@ -171,29 +168,28 @@ public class PackageRegistryStepdefsTest extends AbstractPackageRegistryTest {
         assertTrue(finder.findPackageByTrackingNumber(trackingId).isPresent());
     }
 
-    //@Then("^there is (\\d+) item in the package list and the package with trackingId (.*) does not exist anymore$")
-    //public void checkDeletePackage(int number,  String trackingId){
-    //    assertEquals(number, finder.findAllPackages().size());
-    //    assertFalse(finder.findPackageByTrackingNumber(trackingId).isPresent());
-    //}
-//
-    //@Then("^the package with trackingId (.*) has now (.*) as supplier$")
-    //public void checkEditPackage(String trackingId, String supplier){
-    //    Optional<Package> packageOptional = finder.findPackageByTrackingNumber(trackingId);
-    //    if(!packageOptional.isPresent())
-    //        fail();
-    //    assertEquals(supplier, packageOptional.get().getSupplier().getName());
-    //}
-//
-    //@Then("^the package is found$")
-    //public void checkFoundPackage(){
-    //    assertTrue(packageFound.isPresent());
-    //}
-//
-    //@Then("^the package is not found$")
-    //public void checkNotFoundPackage(){
-    //    assertFalse(packageFound.isPresent());
-    //}
+    @Then("^the package with trackingId (.*) does not exist anymore$")
+    public void checkDeletePackage(String trackingId){
+        assertFalse(finder.findPackageByTrackingNumber(trackingId).isPresent());
+    }
+
+    @Then("^the package with trackingId (.*) has now (.*) as supplier$")
+    public void checkEditPackage(String trackingId, String supplier){
+        Optional<Package> packageOptional = finder.findPackageByTrackingNumber(trackingId);
+        if(!packageOptional.isPresent())
+            fail();
+        assertEquals(supplier, packageOptional.get().getSupplier().getName());
+    }
+
+    @Then("^the package is found$")
+    public void checkFoundPackage(){
+        assertTrue(packageFound.isPresent());
+    }
+
+    @Then("^the package is not found$")
+    public void checkNotFoundPackage(){
+        assertFalse(packageFound.isPresent());
+    }
 
     @cucumber.api.java.After
     public void cleaningUp(){
--- j2e/PackageRegistry/src/test/resources/features/EditPackage.feature
+++ j2e/PackageRegistry/src/test/resources/features/EditPackage.feature
@@ -1,13 +1,13 @@
-#Feature: Edit package
-#  This feature support the way a manutentionnaire edits a package
-#
-#  Background:
-#    Given some suppliers LePosta and Nozama and packages with trackingIds UIDL45A UIDL45B
-#
-#  Scenario: There is 2 packages in the system and 1 edit
-#    When the manutentionnaire edit the package with trackingId UIDL45A and set Nozama as supplier
-#    Then the package with trackingId UIDL45A has now Nozama as supplier
-#
-#  Scenario: There is 2 packages in the system and 1 deletion
-#    When the gestionnaire wants to edit the package with trackingId UIDL45F and supplier Nozama there is an error
-#    Then there is an exception
+Feature: Edit package
+  This feature support the way a manutentionnaire edits a package
+
+  Background:
+    Given some suppliers LePosta and Nozama and packages with trackingIds UIDL45A UIDL45B
+
+  Scenario: There is 2 packages in the system and 1 edit
+    When the manutentionnaire edit the package with trackingId UIDL45A and set Nozama as supplier
+    Then the package with trackingId UIDL45A has now Nozama as supplier
+
+  Scenario: There is 2 packages in the system and 1 deletion
+    When the gestionnaire wants to edit the package with trackingId UIDL45F and supplier Nozama there is an error
+    Then there is an exception
