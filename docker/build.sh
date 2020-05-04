974b1a6a52acae99b159331f2f56d66ab39bb514
Damien Montoya <daminor26@gmail.com>
2020-05-04 23:23:53 +0200

    docker done injenkins #44

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Be sure that you do not have started mvn tomee:run or mono server.exe
 
-./clients/build.sh
-./dd/build.sh
-./partners/build.sh
+cd clients
+./build.sh
+cd ..
+
+cd dd
+./build.sh
+cd ..
+
+cd partners
+./build.sh
+cd ..
--- docker/clients/build.sh
+++ docker/clients/build.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-./boss_cli/build.sh
-./chargeur_cli/build.sh
-./garagiste_cli//build.sh
-./gestionnaire_cli/build.sh
-./manutentionnaire_cli/build.sh
-./service_client_cli/build.sh
-./integration_tests_cli/build.sh
+cd boss_cli
+./build.sh
+cd ..
+
+cd chargeur_cli
+./build.sh
+cd ..
+
+cd garagiste_cli
+./build.sh
+cd ..
+
+cd gestionnaire_cli
+./build.sh
+cd ..
+
+cd manutentionnaire_cli
+./build.sh
+cd ..
+
+cd service_client_cli
+./build.sh
+cd ..
+
+cd integration_tests_cli
+./build.sh
+cd ..
